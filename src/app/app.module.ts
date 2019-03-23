15a57b0103eb0a2e574596a67a15cea11a846b66
ishizaki <gabriel.ishizaki@hotmail.com>
2019-03-23 11:44:34 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { NewsComponent } from './news/news.component';
+import { AlunoProfileComponent } from './aluno-profile/aluno-profile.component';
+import { ProfessorProfileComponent } from './professor-profile/professor-profile.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuBarComponent,
+    NewsComponent,
+    AlunoProfileComponent,
+    ProfessorProfileComponent,
+    ProfessoresComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
